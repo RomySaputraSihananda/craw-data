394d76d7f28662aa3a6764d0409e30ec5bea799a
romys <romysaputrasihanandaa@gmail.com>
2024-03-01 15:09:13 +0700

    update

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,8 @@ import click
 from src.helpers import ConnectionS3, ConnectionKafka
 @click.command()
 def s32k():
-    conn = ConnectionKafka(['kafka01.research.ai:9092', 'kafka02.research.ai:9092', 'kafka03.research.ai:9092'])
+    # conn = ConnectionKafka(['kafka01.research.ai:9092', 'kafka02.research.ai:9092', 'kafka03.research.ai:9092'])
+    conn = ConnectionKafka(['localhost:9092'])
     for prefix in ConnectionS3.get_all_prefix('data/data_raw/bpom/product/'):
         conn.send('data-knowledge-repo-general_10', ConnectionS3.get_content(prefix))
 
