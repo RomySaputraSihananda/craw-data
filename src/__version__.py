512497a48a0cffc80e46227b645f0f1cc7526c0b
romys <romysaputrasihanandaa@gmail.com>
2024-03-07 16:22:12 +0700

    add __version__

---
+++ src/__version__.py
@@ -0,0 +1,8 @@
+
+__title__ = "Engine Crawler Data"
+__description__ = "Engine Crawling Biasa."
+__url__ = "https://github.com/romysaputrasihananda/craw-data"
+__autor__ = 'romy'
+__version__ = '4.1.7'
+__author_email__ = "romysaputrasihanandaa@gmail.com"
+__license__ = "MIT"
