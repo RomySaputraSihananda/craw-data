567876f948c319baa422c222a82b953f886a4056
romys :) <romysaputrasihanandaa@gmail.com>
2024-03-28 13:15:32 +0700

    update lamudi engine

--- src/controller/main.py
+++ src/controller/main.py
@@ -4,7 +4,11 @@ from fastapi import FastAPI
 from .test import Test
 from src.controller.lamudi import LamudiController
 
-app: FastAPI = FastAPI(title='Service', version='v1.0.0', description='Bukan service BE')
+app: FastAPI = FastAPI(
+    title='Lamudi Service', 
+    version='v0.0.1', 
+    description='rest api untuk mengambil data ruko dan kos dari aplikasi [lamudi.co.id](https://lamudi.co.id)',
+)
 
 # app.include_router(Test().router, prefix="/api/v1/test", tags=["Google Search image"])
 app.include_router(LamudiController().router, prefix="/api/v1/lamudi", tags=["Lamudi"])
