77222325cf4f208b93a1280f3f4e32b5f3d87ec7
romys :) <romysaputrasihanandaa@gmail.com>
2024-04-16 12:33:28 +0700

    update

--- src/controller/main.py
+++ src/controller/main.py
@@ -3,13 +3,24 @@ import uvicorn
 from fastapi import FastAPI
 from src.controller.lamudi import LamudiController
 
-app: FastAPI = FastAPI(
-    title='Lamudi Service', 
-    version='v0.0.1', 
-    description='rest api untuk mengambil data ruko dan kos dari aplikasi [lamudi.co.id](https://lamudi.co.id)',
-)
+class Controllers:
+    def __init__(self, **kwargs) -> None:
+        self.__app: FastAPI = FastAPI(
+            title=kwargs.get('title'), 
+            version=kwargs.get('version'),
+            description=kwargs.get('description')
+        )
+    
+    def lamudi(self, **kwargs) -> FastAPI:
+        self.__app.include_router(LamudiController().router, prefix="/api/v1/lamudi", tags=["Lamudi"])
+        uvicorn.run(self.__app, host='0.0.0.0', port=kwargs.get('port', 4444))
+        
 
-app.include_router(LamudiController().router, prefix="/api/v1/lamudi", tags=["Lamudi"])
+# app.include_router(LamudiController().router, prefix="/api/v1/lamudi", tags=["Lamudi"])
 
 if(__name__ == "__main__"):
-    uvicorn.run(app, host='0.0.0.0', port=4444)
+    Controllers(
+        title='Lamudi Service', 
+        version='v0.0.1',
+        description='rest api untuk mengambil data ruko dan kos dari aplikasi [lamudi.co.id](https://lamudi.co.id)',
+    ).lamudi()
