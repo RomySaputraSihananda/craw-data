8e6dad795d3d660fe38488819c15ec6910b9fe88
romys :) <romysaputrasihanandaa@gmail.com>
2024-05-31 15:41:56 +0700

    update

--- src/controller/uiacidevent/uiacidevent.py
+++ src/controller/uiacidevent/uiacidevent.py
@@ -2,8 +2,9 @@ from http import HTTPStatus
 from datetime import datetime
 from fastapi import APIRouter, Query
 from fastapi.responses import JSONResponse
+from time import time
 
-from src.helpers import BodyResponse
+from src.helpers import BodyResponse, Datetime
 from src.library.dataDivtik import AbstractUiacidEvent
 
 class UiacidEventController(AbstractUiacidEvent): 
@@ -19,6 +20,15 @@ class UiacidEventController(AbstractUiacidEvent):
             year: int = Query(default=datetime_now.year, description='year of event'),
     ) -> JSONResponse:
         events= await super()._get_event_by_date(year, month)
+
+        headers: dict = {
+            "source": (link := "https://www.ui.ac.id/events/list"),
+            "domain": (link_split := link.split('/')[:-1])[2],
+            "data_name": "data_event",
+            "tag": [*link_split[2:], 'data_event'],
+            "crawling_time": Datetime.now(),
+            "crawling_time_epoch": int(time())
+        } 
         if(not events): return JSONResponse(
             content=BodyResponse(
                 HTTPStatus.NOT_FOUND, 
@@ -31,8 +41,24 @@ class UiacidEventController(AbstractUiacidEvent):
         return JSONResponse(
             content=BodyResponse(
                 HTTPStatus.OK, 
-                events, 
-                message=f'list of events in {year}-{month}', 
+                [
+                    {
+                        "link_source": (url := event["Event"]["url"]),
+                        "event_name": event["Event"]["name"],
+                        "event_organizer": event["Organization"]["name"],
+                        "category": 'event',
+                        "start_date": (start_date_split := event["Event"]["startDate"].split('T'))[0],
+                        "end_date": (end_date_split := event["Event"]["endDate"].split('T'))[0],
+                        "start_time": '.'.join(start_date_split[1].split(':', 2)[:2]),
+                        "end_time": '.'.join(end_date_split[1].split(':', 2)[:2]),
+                        "event_tag": [*headers['tag'], *url.split('/')[:-1][2:]],
+                        "event_description": event["Event"]["description"],
+                        "social_media": None,
+                        "link_image": event["ImageObject"]["url"]
+                    } for event in events
+                ], 
+                message=f'list of events in {year}-{month}',
+                **headers
             ).__dict__, 
             status_code=HTTPStatus.OK
         )
