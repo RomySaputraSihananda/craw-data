24635df4145792e2e6ab22d5849d9669ab078de4
romysaputrasihananda <romysaputrasihanandaa@gmail.com>
2024-09-02 12:03:57 +0700

    update binapemdes

--- src/library/shinta/binapemdes/binapemdes.py
+++ src/library/shinta/binapemdes/binapemdes.py
@@ -26,7 +26,7 @@ class BinaPemdes:
         self.__get_cookies()
 
         self.__redis: Redis = Redis(
-            # '192.168.20.175',
+            '192.168.20.175',
             db=2,
             decode_responses=True
         )
