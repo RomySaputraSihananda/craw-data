89d5bb8ea11a7bb4e9af711304a1eeaa626e6955
romysaputrasihananda <romysaputrasihanandaa@gmail.com>
2024-10-03 12:14:45 +0700

    add kemenporadataset & kemendesapublikasi

---
+++ src/library/kelengkapan/kemendesapublikasi/kemendesapublikasi.py
@@ -0,0 +1,114 @@
+import requests
+from bs4 import BeautifulSoup 
+from metadata import Metadata
+from concurrent.futures import ThreadPoolExecutor, Future
+from src.helpers import ConnectionS3, Iostream
+
+clean = lambda x: x.lower().replace(' ', '_').replace(' ', '_').replace('-', '_').replace('/', '_').replace('-', '_')
+
+class KemendesaPublikasi:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def get_soup(html) -> BeautifulSoup:
+        return BeautifulSoup(html, 'html.parser')
+    
+    @staticmethod
+    def get_format(link):
+        name, format = (file_name := link.split('/')[-1]).rsplit('.', 1)
+        return {
+            'name': name,
+            'format': format,
+            'file_name': file_name
+        }
+    
+    def __download(self, img, path):
+        final_path = f'{path.replace("data_descriptive", "data_gambar")}/{clean(img["format"])}/{clean(img["file_name"])}'
+        ConnectionS3.upload_content(requests.get(img['link'], verify=False).content, final_path.replace('s3://ai-pipeline-raw-data/',''), 'ai-pipeline-raw-data')
+        return final_path
+
+    def _get_cards(self, page):
+        response = requests.get(
+            'https://kemendesa.go.id/berita/content/infografis_kdpdtt',
+            params={
+                    'key': '',
+                    'per_page': (page - 1) * 9,
+                },
+            verify=False
+        )   
+        soup = self.get_soup(response.text)
+
+        return [
+                {
+                    'link': a["href"],
+                    'title': a["title"]
+                } for a in soup.select('.thumbnail a')
+            ]
+    
+    def _process_card(self, card):
+        print('starrttttoooooooooo')
+        response = requests.get(
+            (link := card['link']),
+            verify=False
+        )   
+        soup = self.get_soup(response.text)
+        metadata = Metadata(
+            link=link,
+            source=(link_split := link.split('/')[2:])[0],
+            tags=[
+                *link_split,
+                'publikasi',
+                'infografis'
+            ],
+            title=card['title'],
+            update_schedule='non-periodic', 
+            category='infografis',
+            path_data_raw=[
+                f's3://ai-pipeline-raw-data/data/data_descriptive/kemendesa/infografis/json/{clean(card["title"])}.json'
+            ],
+            data={
+                **card,
+                'images': [
+                    {
+                        'link': (link_img := img['src']),
+                        **self.get_format(link_img)
+                    } for img in soup.select('.infografis img')
+                ]
+            },
+        )
+        metadata.path_data_raw.extend(
+            [
+                self.__download(
+                    image,
+                    metadata.path_data_raw[0].split('/json')[0]
+                ) for image in metadata.data["images"]
+            ]
+        )
+
+        ConnectionS3.upload(metadata.dict, metadata.path_data_raw[0].replace('s3://ai-pipeline-raw-data/',''), 'ai-pipeline-raw-data')
+        # Iostream.write_json(metadata.dict, metadata.path_data_raw[0].replace('s3://ai-pipeline-raw-data/',''))
+
+    def get_data(self):
+        page = 1
+        while(True):
+            cards = self._get_cards(page)
+            if(not cards): break
+            with ThreadPoolExecutor(max_workers=5) as ex:
+                futures: list[Future] = []
+                for card in cards:
+                    futures.append(
+                        ex.submit(
+                            self._process_card,
+                            card
+                        )
+                    )
+                
+                for future in futures:
+                    future.result()
+
+            page += 1
+
+
+if(__name__ == '__main__'):
+    KemendesaPublikasi().get_data()
---
+++ src/library/kelengkapan/kemenporadataset/kemenporadataset.py
@@ -0,0 +1,119 @@
+import json
+import requests
+
+from requests import Session
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from metadata import Metadata
+from src.helpers import Datetime, ConnectionS3
+
+clean = lambda x: x.lower().replace(' ', '_').replace(' ', '_').replace('-', '_').replace('/', '_').replace('-', '_')
+
+
+class KemenporaDataset:
+    def __init__(self) -> None:
+        self.__session = Session()
+        self.__session.headers.update({
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+        })
+
+    @staticmethod
+    def get_soup(html) -> BeautifulSoup:
+        return BeautifulSoup(html, 'html.parser')
+
+    @staticmethod
+    def get_graph_by_type(data, type) -> dict:
+        return [d for d in data['@graph'] if d['@type'] == type]
+
+    def __download(self, file, path):
+        match(format := file['schema:encodingFormat'].lower()):
+            case 'xlsx' | 'csv': ...
+            case 'pdf': path = path.replace('data_statistics', 'data_descriptive')
+            case _: return
+
+        final_path = f'{path}/{format}/{clean(file['schema:name'])}'
+        ConnectionS3\
+            .upload_content(
+                self.__session.get(
+                    file['schema:url']
+                ).content, 
+                final_path.replace('s3://ai-pipeline-raw-data/',''), 
+                'ai-pipeline-raw-data'
+            )
+        
+        return final_path
+
+    def _process_data(self, url):
+        data = self._get_detail_data(url)
+        
+        metadata = Metadata(
+            link='https://satudata.kemenpora.go.id' + url,
+            tags=[
+                'satudata.kemenpora.go.id',
+                *url.split('/')
+            ],
+            source='satudata.kemenpora.go.id',
+            title=(title := (dataset := self.get_graph_by_type(data, 'schema:Dataset')[0])['schema:name']),
+            create_date=Datetime.utc(dataset['schema:datePublished']),
+            update_date=Datetime.utc(dataset['schema:dateModified']),
+            data=data,
+            desc=dataset.get('schema:description'),
+            category=(category := thing[0]['schema:name'] if (thing := self.get_graph_by_type(data, 'schema:Thing')) else 'lainnya'),
+            path_data_raw=[
+                f's3://ai-pipeline-raw-data/data/data_statistics/kemenpora/dataset/{category}/json/{clean(title)}.json'
+            ],
+            update_schedule='yearly'
+        )
+
+        metadata.path_data_raw.extend([
+            self.__download(
+                file,
+                metadata.path_data_raw[0].split('/json')[0]
+            ) for file in self.get_graph_by_type(data, 'schema:DataDownload')
+        ]) 
+
+        ConnectionS3\
+            .upload(
+                metadata.dict,
+                metadata.path_data_raw[0].replace('s3://ai-pipeline-raw-data/',''), 
+                'ai-pipeline-raw-data'
+            )
+
+    def _get_detail_data(self, url):
+        response = self.__session.get(
+            'https://satudata.kemenpora.go.id' + url
+        )   
+        soup = self.get_soup(response.text)
+        return json.loads(soup.select_one('script[type="application/ld+json"]').string)
+
+    def _get_url(self, page):
+        response = self.__session.get(
+            'https://satudata.kemenpora.go.id/dataset/',
+            params={
+                'page': page
+            }
+        )
+        soup = self.get_soup(response.text)
+        return [
+            li.select_one('a')["href"] for li in soup.select('.dataset-item')
+        ]
+    
+    def get_data(self):
+        urls = self._get_url(1)
+
+        with ThreadPoolExecutor(max_workers=5) as ex:
+            futures = []
+            for url in urls:
+                futures.append(
+                    ex.submit(
+                        self._process_data,
+                        url
+                    )
+                )
+
+            for future in futures:
+                future.result()
+
+
+if(__name__ == '__main__'):
+    KemenporaDataset().get_data()
